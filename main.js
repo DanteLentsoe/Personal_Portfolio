60c263f4aba4c81b1090f9b5be5d91380ee4fb53
Dante L. Lentsoe <65385487+DanteLentsoe@users.noreply.github.com>
2020-10-18 13:59:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-function narBar(){
-    const responsiveBar = document.querySelector(".bar-nav");
-    const navLinks = document.querySelector(".nav-link-bar");
-    const links = document.querySelectorAll(".nav-link-bar li");
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menuitems = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
 
-    responsiveBar.addEventListener('click', () => {
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
 
-        navLinks.classList.toggle('open');
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 80) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
 
-    });
-};
+menuitems.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
